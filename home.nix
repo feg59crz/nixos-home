9b67aa31245db44df34e363677dbd93b8508a4ad
feg59crz <fernandogomescruz@gmail.com>
2022-07-11 17:06:25 -0300

    home-manager modules
    git module, packages module and default.nix

--- home.nix
+++ home.nix
@@ -1,10 +1,25 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "eee";
-  home.homeDirectory = "/home/eee";
+  config = {
+    home.username = "eee";
+    home.homeDirectory = "/home/eee";
 
-  home.stateVersion = "22.05";
+    home.stateVersion = "22.05";
 
-  programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
+  };
+  imports = [ ./modules/default.nix ];
+  config.modules = {
+
+    packages.enable = true; 
+
+    git.enable = true;
+    
+    # wm
+    # awesome.enable = true;
+
+    # terminal
+    # kitty.enable = true;
+  };
 }
